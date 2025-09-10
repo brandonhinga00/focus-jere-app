b8cd3267283745410da80804359263b0aabf878c
brandonhinga00 <brandonhinga@gmail.com>
2025-09-10 02:44:01 -0300

    v1.4.6

--- src/components/TaskBlock.tsx
+++ src/components/TaskBlock.tsx
@@ -207,14 +207,14 @@ const TaskBlock: React.FC<TaskBlockProps> = ({ task, isCurrent, onEdit, onToggle
             style={{ opacity: opacity }}
             aria-hidden="true"
           >
-            <div className={`flex items-center pl-6 text-white transition-opacity duration-100 ${deltaX < 0 ? 'opacity-100' : 'opacity-0'}`}>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-                  <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm4 0a1 1 0 012 0v6a1 1 0 11-2 0V8z" clipRule="evenodd" />
-                </svg>
-            </div>
-            <div className={`flex items-center pr-6 text-white transition-opacity duration-100 ${deltaX > 0 ? 'opacity-100' : 'opacity-0'}`}>
+            <div className={`flex items-center pl-6 text-white transition-opacity duration-100 ${deltaX > 0 ? 'opacity-100' : 'opacity-0'}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                </svg>
+            </div>
+            <div className={`flex items-center pr-6 text-white transition-opacity duration-100 ${deltaX < 0 ? 'opacity-100' : 'opacity-0'}`}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm4 0a1 1 0 012 0v6a1 1 0 11-2 0V8z" clipRule="evenodd" />
                 </svg>
             </div>
           </div>
